96612e9338364bf9fd554ee32dda10c7b267132c
SASHANK-N <sashanknaredla@gmail.com>
2025-06-19 15:21:58 +0530

    Setup Node.js Express server for static HTML deployment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
